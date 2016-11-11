e80fa57cda667be0364c43f209abead734a1a8ae
David Barajas <david@netlandish.com>
2016-11-11 16:34:56 -0600

    sql2: Add employee_hobby table and rows.

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -16,12 +16,22 @@ CREATE TABLE employee_department (
 	description TEXT
 );
 
-
-CREATE TABLE employee_hobby (
-);
-
 -- adding the foreign key to relate employee with his/her department
 ALTER TABLE employee ADD COLUMN id_employee_department INTEGER NOT NULL REFERENCES employee_department;
+
+
+CREATE TABLE employee_hobby (
+	id SERIAL NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE,
+	description TEXT
+);
+
+CREATE TABLE employee__employee_hobby (
+	id SERIAL NOT NULL PRIMARY KEY,
+	employee_id INTEGER NOT NULL REFERENCES employee,
+	employee_hobby_id INTEGER NOT NULL REFERENCES employee_hobby,
+	UNIQUE (employee_id, employee_hobby_id)
+);
 
 INSERT INTO employee_department (name, description) values ('IT', 'The best one.');
 INSERT INTO employee_department (name, description) values ('Sales', '...');
@@ -31,3 +41,7 @@ INSERT INTO employee_department (name, description) values ('Fanantial', '...');
 INSERT INTO employee_department (name, description) values ('Marketing', '...');
 
 INSERT INTO employee (first_name, last_name, id_employee_department) values ('Alan', 'Turing', 1), ('Juan', 'Perez', 1), ('Laura', 'Salum', 2), ('Alejandro', 'Thomas', 3);
+
+INSERT INTO employee_hobby (name, description) VALUES ('Music', '...'), ('Movies', '...'), ('Soccer', 'A sport that ...');
+
+INSERT INTO employee__employee_hobby (employee_id, employee_hobby_id) values (1, 1), (1, 2), (2, 2), (2, 3), (3, 1), (3, 3), (4, 2), (4, 3);
